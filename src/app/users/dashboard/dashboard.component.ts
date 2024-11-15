216e514af2e4f41edb4a403bb2c05a481ec858cb
ngautamkramer <ngautam@kramerav.com>
2024-11-15 12:45:41 +0530

    4th

--- src/app/users/dashboard/dashboard.component.ts
+++ src/app/users/dashboard/dashboard.component.ts
@@ -1,44 +1,114 @@
-import { HttpClientModule } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewChild } from '@angular/core';
 import { RouterLink, RouterModule } from '@angular/router';
-import Chart from 'chart.js/auto';
+import { Chart, ChartData, ChartEvent } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+import { ApiService } from '../../services/api.service';
+import { CommonModule } from '@angular/common';
+import { MatSelectChange, MatSelectModule } from '@angular/material/select';
+
+Chart.register(ChartDataLabels);
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [RouterLink, RouterModule, HttpClientModule],
+  imports: [BaseChartDirective, RouterModule, RouterLink, CommonModule, MatSelectModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
+  providers: [ApiService],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DashboardComponent {
-  
-  chart: any = [];
 
-  constructor() {}
+  yearsList: any = [];
+  yearCustomerData: any = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  selectedYear: number = new Date().getFullYear();
+
+  constructor(private apiService: ApiService){}
+
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective<'bar'> | undefined;
+
+  public barChartType = 'bar' as const;
+
+  public barChartData: ChartData<'bar'> = {
+    labels: ["January","February","March","April","May","June","July","August","September","October","November","December"],
+    datasets: [
+      { data: this.yearCustomerData, label: 'Customers' },
+    ]
+  };
+
+  // events
+  public chartClicked({
+    event,
+    active,
+  }: {
+    event?: ChartEvent;
+    active?: object[];
+  }): void {
+    //console.log(event, active);
+  }
+
+  public chartHovered({
+    event,
+    active,
+  }: {
+    event?: ChartEvent;
+    active?: object[];
+  }): void {
+   // console.log(event, active);
+  }
 
 
-  ngOnInit() {
-
-    this.chart = new Chart('canvas', {
-      type: 'bar',
-      data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [
-          {
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            borderWidth: 1,
-          },
-        ],
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true,
-          },
-        },
-      },
+  setDataOnChart(data: any) {
+    let sampleData: any = ["01","02","03","04","05","06","07","08","09","10","11","12"];
+    let chartSampleData: any = [0,0,0,0,0,0,0,0,0,0,0,0];
+    sampleData.forEach((emptyArray: any, index: number) => {
+      let searchString = this.selectedYear + '-' + emptyArray;
+      let checkArray:any = [];
+      data.forEach((cst_data: any) => {
+        if(cst_data.created_at.indexOf(searchString) > -1){
+          checkArray.push(cst_data);
+        }
+      });
+      chartSampleData[index] = checkArray.length;
     });
+
+    this.barChartData.datasets[0].data = chartSampleData;
+
+    this.chart?.update();
+  }
+
+
+  getAllCustomers(): any {
+    let userInfo = this.apiService.getUserInfo(); 
+    if(userInfo.user_id){   
+      this.apiService.getCustomers(userInfo.user_id).subscribe({
+        next: (response) => {
+          this.setDataOnChart(response.data);
+        },
+        error: (error) => {
+          console.error('Error during POST:', error.message);
+        }
+      });
+    }
+  }
+  
+  setAllYearsInDropdown(startYear:number){
+    const currentYear = new Date().getFullYear();
+    for (let year = startYear; year <= currentYear; year++) {
+      this.yearsList.push(year)
+    }
+    this.yearsList.reverse();
+  }
+
+  ngOnInit(): void {
+    this.getAllCustomers();
+    this.setAllYearsInDropdown(2015);
+  }
+
+  onSelectionChange(event: MatSelectChange): void {
+    this.selectedYear = event.value;
+    this.getAllCustomers();
   }
 
 }
